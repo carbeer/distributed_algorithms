8275a296f921cd5c7480deb53f62333577179117
carbee <mail@carolin.beer>
2018-10-15 16:03:30 +0200

    added filewriting and crash procedure

--- src/main/java/daProc/Process.java
+++ src/main/java/daProc/Process.java
@@ -14,33 +14,42 @@ public class Process {
 	//Messages sent but not acked yet
 	private ArrayList<Message> sentMsg;
 	//
-	private int id;
-	private String ip;
-	private int port;
-	private String outPath;
+	final private int id;
+	final private String ip;
+	final private int port;
 	private DatagramSocket socket;
 	private boolean crashed;
 	private int seqNumber;
 	private ArrayList<Peer> peers;
+	// Never use the FileWriter until the process crashes.
+	private FileWriter writer;
+	// Add logs here.
+	private String logs;
+
 	public Process(int id, String membership) {
-		
 		this.id = id;
 		File membershipPath = new File(System.getProperty("user.dir") + "/src/main/java/daProc/" + membership + ".txt");
 		String[] processParam = readMembership(membershipPath, id);
 		this.ip = processParam[1];
 		this.port = Integer.valueOf(processParam[2]);
+		this.logs = "";
+		this.peers = getPeers(membershipPath, id);
+		this.crashed = false;
+		this.seqNumber = 1;
 
-		//create outPath
-		this.outPath = "da_proc_" + this.id;
 		try {
 			this.socket = new DatagramSocket(this.port);
 		} catch (java.net.SocketException e) {
 			System.out.println("Error while creating a socket at port " + this.port);
 			e.printStackTrace();
 		}
-		this.peers = getPeers(membershipPath, id);
-		this.crashed = false;
-		this.seqNumber = 1;
+		try {
+			this.writer = new FileWriter(System.getProperty("user.dir") + "/logs/da_proc_" + this.id);
+		} catch (java.io.IOException e) {
+			System.out.println("Error while creating the file writer");
+			e.printStackTrace();
+		}
+		// receive();
 	}
 	
 
@@ -58,6 +67,8 @@ public class Process {
 					e.printStackTrace();
 				}
 			}
+			this.logs = String.format("%sb %s\n", this.logs, seqNumber);
+			System.out.println("Broadcast: b " + seqNumber );
 			seqNumber++;
 		}
 	}
@@ -80,9 +91,9 @@ public class Process {
 				// int port = packet.getPort();
 
 				// Read data from packet
-				String received = new String(packet.getData(), 0, packet.getLength());
-				System.out.println("Received packet with the following content: " + received);
-				// TODO: Write to file
+				String receivedSeqNr = new String(packet.getData(), 0, packet.getLength());
+				this.logs = String.format("%sd %s:%d %s\n", this.logs, packet.getAddress().toString(), packet.getPort(), receivedSeqNr);
+				System.out.println("Received packet: " + String.format("d %s:%d %s", packet.getAddress().toString(), packet.getPort(), receivedSeqNr));
 
 			} catch (java.io.IOException e) {
 				System.out.println("Error while receiving DatagramPacket");
@@ -91,11 +102,16 @@ public class Process {
 		}
 	}
 
+	//TODO Anything else necessary here?
 	public void crash() {
 		this.crashed = true;
 		this.socket.close();
-		//just to clean up buffers and log unlogged data/packets/messages before closing
-		//TODO
+		try {
+			writer.write(logs);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static ArrayList<Peer> getPeers(File f, int procID) {
@@ -178,7 +194,6 @@ public class Process {
 			return this.address + ":" + this.port;
 		}
 
-
 		@Override()
 		public boolean equals(Object obj) {
 			if (obj instanceof Peer) {
@@ -188,20 +203,17 @@ public class Process {
 			return false;
 		}
 	}
- 
     
     public static void main(String []args) {
-
     	int processId = Integer.valueOf(args[1]);
     	String membership = args[2];
-    	Process process = new Process(processId, membership);
-    	
-    	
+    	final Process process = new Process(processId, membership);
+
     	Signal.handle(new Signal("SIGINT"), new SignalHandler() {
             public void handle(Signal sig) {
                 System.out.println("Stopping network packet processing...\n");
-            	//crash(); //clean up using crash method
-                System.out.println("Changes logged, exiting now.\n");
+                process.crash(); //clean up using crash method
+                System.out.println("Teardown complete, exiting now.\n");
             	System.exit(0); //kill process
             }
         });
