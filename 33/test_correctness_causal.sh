bd3062dbc4a46fe9ccf18222af7406689d909987
guillaume.caviggia <gc@modif.com>
2018-12-16 16:45:33 +0100

    minor

--- 33/test_correctness_causal.sh
+++ 33/test_correctness_causal.sh
@@ -8,7 +8,7 @@
 
 # time to wait for correct processes to broadcast all messages (in seconds)
 # (should be adapted to the number of messages to send)
-time_to_finish=20
+time_to_finish=10
 
 init_time=2
 
@@ -41,6 +41,14 @@ done
 # leave some time for process initialization
 sleep $init_time
 
+# do some nasty stuff like process crashes and delays
+# example:
+kill -STOP "${da_proc_id[3]}" # pause process 3
+sleep 1
+kill -TERM "${da_proc_id[2]}" # crash process 2
+da_proc_id[2]=""
+kill -CONT "${da_proc_id[3]}" # resume process 3
+
 # start broadcasting
 for i in `seq 1 5`
 do
@@ -48,6 +56,15 @@ do
 	kill -USR2 "${da_proc_id[$i]}"
     fi
 done
+
+# do some more nasty stuff
+# example:
+kill -TERM "${da_proc_id[4]}" # crash process 4
+da_proc_id[4]=""
+kill -STOP "${da_proc_id[1]}" # pause process 1
+sleep 0.5
+kill -CONT "${da_proc_id[1]}" # resume process 1
+
 
 # leave some time for the correct processes to broadcast all messages
 sleep $time_to_finish
@@ -69,6 +86,6 @@ do
 done
 
 # check logs for correctness
-./check_output.sh 1 2 3 4 5
+./check_output.sh 1 3 5
 
 echo "Correctness test done."
